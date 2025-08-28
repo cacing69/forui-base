4e9fdac724ec4ce7620a20f9cc3610dde8bac37c
Ibnul Mutaki <36250619+cacing69@users.noreply.github.com>
2025-08-28 16:36:19 +0700

    feat: prepare notifier companies

---
+++ lib/features/app/presentation/screens/cctv/app_cctv_list_company_notifier.dart
@@ -0,0 +1,34 @@
+import 'package:forui_base/shared/data/models/api_cctv/company.dart';
+import 'package:forui_base/shared/data/models/api_cctv/company_query.dart';
+import 'package:forui_base/shared/data/models/api_cctv/t_response.dart';
+import 'package:forui_base/shared/domain/usecases/api_cctv/companies_usecase.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'app_cctv_list_company_notifier.g.dart';
+
+@Riverpod(keepAlive: true)
+class AppCctvListCompanyNotifier extends _$AppCctvListCompanyNotifier {
+  @override
+  AsyncValue<TResponse<List<Company>>?> build() {
+    return AsyncValue.loading();
+  }
+
+  Future<void> perform(CompanyQuery params) async {
+    state = AsyncValue.loading();
+
+    final result = await ref.read(companiesUsecaesProvider).call(params);
+
+    result.fold(
+      (failure) {
+        state = AsyncValue.error(failure, StackTrace.current);
+      },
+      (data) {
+        state = AsyncValue.data(data);
+      },
+    );
+  }
+
+  void reset() {
+    state = const AsyncValue.data(null);
+  }
+}
---
+++ lib/features/app/presentation/screens/cctv/app_cctv_list_company_notifier.g.dart
@@ -0,0 +1,79 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'app_cctv_list_company_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+@ProviderFor(AppCctvListCompanyNotifier)
+const appCctvListCompanyNotifierProvider =
+    AppCctvListCompanyNotifierProvider._();
+
+final class AppCctvListCompanyNotifierProvider
+    extends
+        $NotifierProvider<
+          AppCctvListCompanyNotifier,
+          AsyncValue<TResponse<List<Company>>?>
+        > {
+  const AppCctvListCompanyNotifierProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'appCctvListCompanyNotifierProvider',
+        isAutoDispose: false,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$appCctvListCompanyNotifierHash();
+
+  @$internal
+  @override
+  AppCctvListCompanyNotifier create() => AppCctvListCompanyNotifier();
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(AsyncValue<TResponse<List<Company>>?> value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride:
+          $SyncValueProvider<AsyncValue<TResponse<List<Company>>?>>(value),
+    );
+  }
+}
+
+String _$appCctvListCompanyNotifierHash() =>
+    r'e238aabde4816ae277f2007c7529376bcf7cfbfc';
+
+abstract class _$AppCctvListCompanyNotifier
+    extends $Notifier<AsyncValue<TResponse<List<Company>>?>> {
+  AsyncValue<TResponse<List<Company>>?> build();
+  @$mustCallSuper
+  @override
+  void runBuild() {
+    final created = build();
+    final ref =
+        this.ref
+            as $Ref<
+              AsyncValue<TResponse<List<Company>>?>,
+              AsyncValue<TResponse<List<Company>>?>
+            >;
+    final element =
+        ref.element
+            as $ClassProviderElement<
+              AnyNotifier<
+                AsyncValue<TResponse<List<Company>>?>,
+                AsyncValue<TResponse<List<Company>>?>
+              >,
+              AsyncValue<TResponse<List<Company>>?>,
+              Object?,
+              Object?
+            >;
+    element.handleValue(ref, created);
+  }
+}
+
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
