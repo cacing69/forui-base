9ee711f6f048153ea0740e36d292efd49c37bbba
Ibnul Mutaki <36250619+cacing69@users.noreply.github.com>
2025-08-30 00:03:09 +0700

    feat: add static path_parameter and query_parameter

---
+++ lib/core/constant/path_parameter_key.dart
@@ -0,0 +1,4 @@
+abstract class PathParameterKey {
+  static String imageUrl = "imageUrl";
+  static String personId = "personId";
+}
---
+++ lib/core/constant/query_parameter_key.dart
@@ -0,0 +1,3 @@
+abstract class QueryParameterKey {
+  static String personId = "personId";
+}
