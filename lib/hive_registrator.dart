9e6790e9b1d88a560d5ceac93fb488b0f4082646
Ibnul Mutaki <36250619+cacing69@users.noreply.github.com>
2025-08-30 20:06:45 +0700

    feat: add hive registrator

--- lib/hive_registrator.dart
+++ lib/hive_registrator.dart
@@ -1,10 +1,14 @@
 import 'package:forui_base/hive_registrar.g.dart';
+import 'package:forui_base/shared/data/models/api_cctv/city.dart';
 import 'package:forui_base/shared/data/models/api_cctv/company.dart';
+import 'package:forui_base/shared/data/models/api_cctv/district.dart';
 import 'package:forui_base/shared/data/models/api_cctv/family.dart';
 import 'package:forui_base/shared/data/models/api_cctv/gojek.dart';
 import 'package:forui_base/shared/data/models/api_cctv/person.dart';
 import 'package:forui_base/shared/data/models/api_cctv/phone.dart';
 import 'package:forui_base/shared/data/models/api_cctv/pln.dart';
+import 'package:forui_base/shared/data/models/api_cctv/province.dart';
+import 'package:forui_base/shared/data/models/api_cctv/village.dart';
 import 'package:forui_base/shared/data/models/hive/t_response_adapter.dart';
 import 'package:hive_ce/hive.dart';
 
@@ -76,6 +80,56 @@ Future<void> hiveRegistrator() async {
       typeId: 1002,
       fromJson: (json) => (json['data'] as List<dynamic>)
           .map((e) => Family.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      toJson: (items) => {'data': items.map((e) => e.toJson()).toList()},
+    ),
+  );
+
+  Hive.registerAdapter(
+    TResponseAdapter<List<Province>>(
+      typeId: 1002,
+      fromJson: (json) => (json['data'] as List<dynamic>)
+          .map((e) => Province.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      toJson: (items) => {'data': items.map((e) => e.toJson()).toList()},
+    ),
+  );
+
+  Hive.registerAdapter(
+    TResponseAdapter<List<City>>(
+      typeId: 1002,
+      fromJson: (json) => (json['data'] as List<dynamic>)
+          .map((e) => City.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      toJson: (items) => {'data': items.map((e) => e.toJson()).toList()},
+    ),
+  );
+
+  Hive.registerAdapter(
+    TResponseAdapter<List<District>>(
+      typeId: 1002,
+      fromJson: (json) => (json['data'] as List<dynamic>)
+          .map((e) => District.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      toJson: (items) => {'data': items.map((e) => e.toJson()).toList()},
+    ),
+  );
+
+  Hive.registerAdapter(
+    TResponseAdapter<List<Village>>(
+      typeId: 1002,
+      fromJson: (json) => (json['data'] as List<dynamic>)
+          .map((e) => Village.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      toJson: (items) => {'data': items.map((e) => e.toJson()).toList()},
+    ),
+  );
+
+  Hive.registerAdapter(
+    TResponseAdapter<List<Company>>(
+      typeId: 1002,
+      fromJson: (json) => (json['data'] as List<dynamic>)
+          .map((e) => Company.fromJson(e as Map<String, dynamic>))
           .toList(),
       toJson: (items) => {'data': items.map((e) => e.toJson()).toList()},
     ),
